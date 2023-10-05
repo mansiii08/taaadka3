e86cbb48e956c54fa998a43c4550b73d4cff03fe
mansiii08 <mansithaker8@gmail.com>
2023-10-04 20:19:07 -0700

    commit

---
+++ echo_saksham.php
@@ -0,0 +1,6 @@
+<?php
+echo "<h2>PHP is Fun, This is Mansi site!</h2>";
+echo "Hello there!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+?>
